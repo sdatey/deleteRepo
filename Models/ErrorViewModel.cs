de7fc7c4a6e9519cde0e2e13c45e4399f863defa
Sachin Datey <sdatey@fmtconsultants.com>
2020-12-04 06:51:11 +0000

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVC_With_DevOps_CI_CD.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
